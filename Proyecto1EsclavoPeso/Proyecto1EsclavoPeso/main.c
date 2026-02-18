810a80274e95c0bc04a7acaca70df3490a55c366
Jorge C. <jorgeluisdejesusc@gmail.com>
2026-02-17 18:07:32 -0600

    Esclavo peso con stepper main.c

--- Proyecto1EsclavoPeso/Proyecto1EsclavoPeso/main.c
+++ Proyecto1EsclavoPeso/Proyecto1EsclavoPeso/main.c
@@ -13,17 +13,86 @@
 #include <util/delay.h>
 #include "I2C/I2C.h"
 #include "HX711/HX711.h"
+#include "PWM0/PWM0.h"
 
-#define SlaveAddress 0x30
-#define FACTOR_CALIBRACION 45.0 // Ajustar seg鷑 calibraci髇 real
+#define SlaveAddress 0x32
+#define FACTOR_CALIBRACION 45.0 // Ajustar seg煤n calibraci贸n real
 
+//Variables para Stepper
 volatile uint16_t pesoEnGramos = 0;
 uint8_t buffer = 0;
 uint8_t byteIndex = 0;
+uint8_t pos_lista = 0;
+uint8_t nueva_pos = 0;
+//Variables para servo
+uint16_t pulso_us = 1000;
+uint16_t pulse_us = 1000;
+uint8_t duty = 63;
+uint8_t cant_volt = 0;
+uint8_t pulso_recibido = 0;
+uint8_t contador = 0;
+uint8_t pwmmanual = 0;
+
+
+const uint8_t secuencia[4] = {
+	0b0011, //Paso 1
+	0b0110, //Paso 2
+	0b1100, //Paso 3
+	0b1001  // Paso 4
+};
+
+
+volatile long posicion_actual = 0;
+volatile uint8_t posicion_objetivo = 0;
+
+
+
+void stepper_step(uint8_t direccion) {
+	static uint8_t paso = 0;
+
+	if(direccion > 0)
+	{
+		paso++;
+		if(paso >= 4) paso = 0;
+	}
+	else
+	{
+		if(paso == 0) paso = 3;
+		else paso--;
+	}
+
+	PORTD = (PORTD & 0x0F) | (secuencia[paso] << 4);
+
+	_delay_ms(3);  // ajusta velocidad
+}
+
+void stepper_update(long destino)
+{
+	long diferencia = destino - posicion_actual;
+
+	if(diferencia > 0)
+	{
+		for(long i = 0; i < diferencia; i++)
+		{
+			stepper_step(1);
+		}
+	}
+	else if(diferencia < 0)
+	{
+		for(long i = 0; i < -diferencia; i++)
+		{
+			stepper_step(-1);
+		}
+	}
+
+	posicion_actual = destino;
+}
+
+
 
 int main(void)
 {
-	DDRD |= (1 << DDD4); // Configuraci髇 pines
+	DDRD |= (1 << DDD4) |(1 << DDD5)| (1 << DDD6) |(1 << DDD7); // Configuraci贸n pines
 	
 	I2C_Slave_Init(SlaveAddress);
 	HX711_init();
@@ -34,18 +103,50 @@ int main(void)
 	
 	while (1)
 	{
-		// Lectura y c醠culo
+		// Lectura y c谩lculo
 		int32_t lecturaRaw = HX711_readAverage_A128(2);
 		float calculo = (float)(lecturaRaw - offset) / FACTOR_CALIBRACION;
 		
 		if (calculo < 0) calculo = 0;
 		
-		// Actualizaci髇 at髆ica de variable global
+		// Actualizaci贸n at贸mica de variable global
 		cli();
-		pesoEnGramos = (uint16_t)calculo;
+		pesoEnGramos = (uint16_t)calculo; 
 		sei();
 		
 		_delay_ms(10);
+		
+		//Entonces aqui va lo del motor
+		
+		
+		if (pesoEnGramos < 15)
+		{
+			
+			stepper_update(0);
+			pos_lista = 1;
+		} else if (pesoEnGramos < 50)
+		{
+			stepper_update(512);
+			pos_lista = 1;
+		} else {
+			stepper_update(1024);
+		}
+		//Todo arriba de esto funciona, por si hay que borrar es de aqui para abajo
+		
+		/*
+		
+		if(pos_lista){
+			pwm0_set_pulse(1000);
+			pos_lista = 0;	
+		} else{
+			
+			pwm0_set_pulse(2000);
+		}
+		*/
+		/*
+		cuando terminen de llegar a su posicion envian un uno a una variable nueva
+		si esta variable nueva es 1 el servo usa pwm para ir de 0 a 180.
+		*/
 	}
 }
 
